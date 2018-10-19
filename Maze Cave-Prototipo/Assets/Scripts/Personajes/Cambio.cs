6f0624e0750c0a8c83151337f62ddba5b26e05bb
RomDrake <rommeloscuro@gmail.com>
2018-10-19 16:47:28 -0400

    primera subida

---
+++ Maze Cave-Prototipo/Assets/Scripts/Personajes/Cambio.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cambio : MonoBehaviour
+{
+    private bool activeAgua;
+    private bool activeBaba;
+    private bool activeAcido;
+
+    public bool ActiveAgua
+    {
+        get
+        {
+            return activeAgua;
+        }
+
+        set
+        {
+            activeAgua = value;
+        }
+    }
+
+    public bool ActiveBaba
+    {
+        get
+        {
+            return activeBaba;
+        }
+
+        set
+        {
+            activeBaba = value;
+        }
+    }
+
+    public bool ActiveAcido
+    {
+        get
+        {
+            return activeAcido;
+        }
+
+        set
+        {
+            activeAcido = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        ActiveAgua = true;
+        ActiveBaba = false; 
+        ActiveAcido = false; 
+}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActiveAgua = true;
+            ActiveBaba = false;
+            ActiveAcido = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActiveAgua = false;
+            ActiveBaba = true;
+            ActiveAcido = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ActiveAgua = false;
+            ActiveBaba = false;
+            ActiveAcido = true;
+        }
+    }
+}
---
+++ Maze Cave-Prototipo/Assets/Scripts/Personajes/MovePersonaje.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePersonaje : MonoBehaviour
+{
+    public float speed = 4f;
+    public Cambio cambioPersonaje;
+    Transform agua;
+    Transform baba;
+    Transform acido;
+    void Start()
+    {
+        cambioPersonaje = GetComponent<Cambio>();
+        agua =  GameObject.FindWithTag("Agua").GetComponent<Transform>();
+        baba = GameObject.FindWithTag("Baba").GetComponent<Transform>();
+        acido = GameObject.FindWithTag("Acido").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if (cambioPersonaje.ActiveAgua && agua.CompareTag("Agua"))
+        {
+            Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            agua.position = Vector3.MoveTowards(agua.position, agua.position + mov, speed * Time.deltaTime);
+        }
+        if (cambioPersonaje.ActiveBaba && baba.CompareTag("Baba"))
+        {
+            Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            baba.position = Vector3.MoveTowards(baba.position, baba.position + mov, speed * Time.deltaTime);
+        }
+        if (cambioPersonaje.ActiveAcido && acido.CompareTag("Acido"))
+        {
+            Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            acido.position = Vector3.MoveTowards(acido.position, acido.position + mov, speed * Time.deltaTime);
+        }
+    }
+}
